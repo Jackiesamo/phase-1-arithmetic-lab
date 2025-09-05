815ad17d93a4f0d040d0574814c16a45758f21e8
jacklyne <jacklyne.owuor@student.moringaschool>
2025-09-05 12:41:52 +0300

    added Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply= num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // mod equals 4
+
+const numbers = new Set([10, 15, 20, 5, 12]);
+const max = Math.max(...numbers);
